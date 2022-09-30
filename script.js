8e8c8c7c5ec10e6e8fd2444818d5dcb9b7b9706f
briabr <filalibria@yahoo.fr>
2022-09-29 20:10:12 -0400

    setting generatepassword function parameters

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ var number;
 var upperCase;
 var lowerCase;
 var specialCharacter;
-var passwordLength
-var userChoice;//var selection
-var capitalLetters
+var passwordLength;
+var userChoice;
+var capitalLetters;
 
 const setsOfcharacters = ["character","number", "letters"];
 character = "!@#$%^&*)()";
 number = "0123456789";
 letters = "abcdefghijklmnopqrstuvwxyz";
 capiltalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-character = "!@#$%^&*)()";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -22,6 +21,17 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+}
+// setting password parameters
+function generatePassword() {
+  // choose number of characters for new password
+  passwordLength = prompt("Enter number of characters between 8-10 for your password");
+  if (!passwordLength) {
+    alert("Please enter the right number of charachters");
+  } else if (length < 8 || length > 10) {
+    // wrong number of characters was entered
+    passwordLength = prompt("password must be from 8-10 characters");
+  }
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
