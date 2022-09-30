df26c74e01b40bd51a188fb343fe00f6cdb6d5d6
briabr <filalibria@yahoo.fr>
2022-09-29 22:32:04 -0400

    added more prompted password criteria

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Assignment code here
+
 // Variables 
 var numbers;
 var upperCase;
@@ -27,11 +28,11 @@ function writePassword() {
 // setting password parameters
 function generatePassword() {
   // choose number of characters for your password
-  passwordLength = prompt("Enter number of characters between 8-128 for your password");
+  passwordLength = prompt("Enter number of characters between 8-128 for your password and at least: one number, one uppercase, one lowercase and one special character");
      if (passwordLength < 8 || passwordLength > 128) {
     // wrong number of characters was entered
     passwordLength = prompt("password must be between 8-128 characters inclusive");
-  } else { // whether the user chose numbers, lowercase, uppercase or specialcase
+  } else { // whether the user  numbers, lowercase, uppercase or specialcase()
     numbers = confirm("Do you want to use numbers in your password?");
     lowerCase = confirm("Do you want to use lowercases in your password?");
     upperCase = confirm("Do you want to use uppercases in your password?");
