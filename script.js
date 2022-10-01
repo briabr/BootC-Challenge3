fd0efe6d7dc8799f03d6f758b4af4c718e9d8fe3
briabr <filalibria@yahoo.fr>
2022-09-30 21:22:33 -0400

    most recent changes

--- script.js
+++ script.js
@@ -1,48 +1,44 @@
 // Assignment code here
 
-// Variables 
+//Variables 
 var numbers;
 var upperCase;
 var lowerCase;
 var specialCharacters;
 var passwordLength;
 var userChoice;
-var capitalLetters;
+// var capitalLetters;
 var charactersCount
+var password = "";
+var userchoice = "";
+var charString = "";
 
-setsofCharacters = ["characters","numbers", "letters"];
-characters = "!@#$%^&*)()";
+
+// setsofCharacters = ["characters","numbers", "letters"];
+specialCharacters = "!@#$%^&*)()";
 numbers = "0123456789";
-letters = "abcdefghijklmnopqrstuvwxyz";
-capiltalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+lowerCase = "abcdefghijklmnopqrstuvwxyz";
+upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  function generatePassword() {
+var passwordLength = prompt("How many characters you want your password to include?");
+while (passwordLength < 8 || passwordLength > 128) {
+  passwordLength = prompt("Password must be between 8-128 characters. How many characters do you want your password to include?");
+} 
+numbersPrompt = confirm("Do you want to use numbers in your password?");
+if (numbersPrompt === true) {
+  charString = charString.concat(numbers);
 }
-// setting password parameters
-function generatePassword() {
-  // choose number of characters for your password
-  passwordLength = prompt("Enter number of characters between 8-128 for your password and at least: one number, one uppercase, one lowercase and one special character");
-     if (passwordLength < 8 || passwordLength > 128) {
-    // wrong number of characters was entered
-    passwordLength = prompt("password must be between 8-128 characters inclusive");
-  } else { // whether the user  numbers, lowercase, uppercase or specialcase()
-    numbers = confirm("Do you want to use numbers in your password?");
-    lowerCase = confirm("Do you want to use lowercases in your password?");
-    upperCase = confirm("Do you want to use uppercases in your password?");
-    specialCharacters = confirm("Do you want to use special characters in your password?");
-  };
-// put the selected characters together
-// randomly select characters from all of the selected characters 
-// put them together
-}
-// var charactersCount = 0
+lowerCase = confirm("Do you want to use lowercases in your password?");
+upperCase = confirm("Do you want to use uppercases in your password?");
+specialCharacters = confirm("Do you want to use special characters in your password?");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+if (!charString) {
+  alert("Please select at least one character type!");
+}
+console.log(charString)
+
+//for loop for randonmly selecting characters from charString 
+// return password
+  }
+ 
