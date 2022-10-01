867198225387da6c3a4090377df7f6a34c37e3aa
briabr <filalibria@yahoo.fr>
2022-09-30 21:38:30 -0400

    added the starter code to the script

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ specialCharacters = "!@#$%^&*)()";
 numbers = "0123456789";
 lowerCase = "abcdefghijklmnopqrstuvwxyz";
 upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 var passwordLength = prompt("How many characters you want your password to include?");
 while (passwordLength < 8 || passwordLength > 128) {
@@ -37,3 +38,16 @@ console.log(charString)
   }
  //for loop for randonmly selecting characters from charString 
 //eturn password
+
+
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword(); 
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// generateBtn.addEventListener("click", writePassword);
+// }
+// Add event listener to generate button
+
